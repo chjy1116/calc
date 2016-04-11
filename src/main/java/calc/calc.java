3e5ae941d54fd04199415355f28d7a52461dfe38
Taehoon Kim <taesic3@gamil.com>
2016-04-11 16:55:57 +0900

    add 기능 및 테스트 코드 추가
    기본 계산 기능

---
+++ src/main/java/calc/calc.java
@@ -0,0 +1,12 @@
+package calc;
+
+public class calc {
+	private int result;
+	public void add(int x, int y) {
+		result =x+y;
+	}
+	
+	public int getResult() {
+		return result;
+	}
+}
---
+++ src/test/java/calc/testcalc.java
@@ -0,0 +1,14 @@
+package calc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testcalc {
+	@Test
+	public void testAdd() {
+		calc calc = new calc();
+		calc.add(10, 20);
+		assertEquals(30, calc.getResult());
+	}
+}
